7303fe05f3b312053613156523cb560605c6612e
nekomikandayo <mrmikan.1417@gmail.com>
2026-05-18 08:55:29 +0900

    fix: remove unsafe GET delete action

---
+++ fuel/app/logs/2026/05/14.php
@@ -0,0 +1,9 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2026-05-14 11:03:04 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2026-05-14 11:03:15 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2026-05-14 11:03:16 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2026-05-14 11:03:22 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2026-05-14 11:03:23 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2026-05-14 11:03:27 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2026-05-14 11:03:31 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
---
+++ fuel/app/logs/2026/05/18.php
@@ -0,0 +1,4 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2026-05-18 08:54:10 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2026-05-18 08:54:19 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
